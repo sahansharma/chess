714d773827e239475da05f24418291d728827d43
sahansharma <sahansharma1000@gmail.com>
2025-10-15 20:52:08 +0545

    Player can move queen diagonally

--- chess/Board.py
+++ chess/Board.py
@@ -1,6 +1,7 @@
 class Board:
     def __init__(self):
         self._pawn_position = 'a1'
+        self._queen_position = 'a1'
 
     def move_pawn(self, from_pos, pos):
         self._pawn_position = pos
@@ -11,5 +12,13 @@ class Board:
     def piece_type(self, pos):
         if self._pawn_position == pos:
             return 'pawn'
+        if self._queen_position == pos:
+            return 'queen'
         return 'empty'
 
+    def add_queen(self, pos):
+        self._queen_position = pos
+
+    def move_queen(self, from_pos, to_pos):
+            self._queen_position = to_pos
+    
--- test/test_board_move_piece.py
+++ test/test_board_move_piece.py
@@ -2,10 +2,8 @@ from chess.Board import Board
 
 
 def test_player_moves_pawn():
-  # given pawn is initially at e3
   board = Board()
   board.add_pawn('e3')
-  # when the player moves pawn from e3 to e4
   board.move_pawn('e3', 'e4')
   # then the pawn is at e4
   assert board.piece_type('e4') == 'pawn'
@@ -18,3 +16,9 @@ def test_player_moves_pawn_away_from_field():
   board.move_pawn('e3', 'e4')
   # then the pawn is not at e3
   assert board.piece_type('e3') == 'empty'
+
+def test_user_moves_queen_diagonally():
+    board = Board()
+    board.add_queen('e3')
+    board.move_queen('e3', 'g5')
+    assert board.piece_type('g5') =='queen'
